c99feb6c267a146caa0da55a74e8946a23cd893a
Leopold Luley <git@leopoldluley.de>
2024-02-11 13:46:44 +0100

    Automatically install dependencies using flatpak-builder.

--- fpk/builder.py
+++ fpk/builder.py
@@ -11,15 +11,48 @@ class Builder:
         self.manifest = manifest
 
     def validate(self):
-        result = subprocess.run(["flatpak-builder", "--version"])
+        result = subprocess.run(["flatpak-builder", "--version"], capture_output=True)
         if result.returncode != 0:
             raise Exception("flatpak-builder could not be found")
 
-    def build(self):
-        subprocess.run(
-            ["flatpak-builder", "--ccache", "--force-clean", self.env, self.manifest],
-            check=True,
+    def install_deps(self) -> bool:
+        result = subprocess.run(
+            [
+                "flatpak-builder",
+                "--install-deps-from=flathub",
+                "--install-deps-only",
+                self.env,
+                self.manifest,
+            ],
+            capture_output=True,
         )
+        if result.returncode != 0:
+            print("Failed to install dependencies:")
+            print("> ", result.stderr.decode())
+            return False
+
+        return True
+
+    def build(self) -> bool:
+        if not self.install_deps():
+            return False
+
+        result = subprocess.run(
+            [
+                "flatpak-builder",
+                "--ccache",
+                "--force-clean",
+                self.env,
+                self.manifest,
+            ],
+            capture_output=True,
+        )
+        if result.returncode != 0:
+            print("Failed run flatpak-builder:")
+            print("> ", result.stderr.decode())
+            return False
+
+        return True
 
     def run(self, cmd: List[str]):
         subprocess.run(
